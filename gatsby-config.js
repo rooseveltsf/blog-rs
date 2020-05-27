411c49980521950e6fca442a231e8f84df556c67
RooseveltSouza <rooseveltsouzav12@gmail.com>
2020-05-27 00:52:52 -0300

    add plugins gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,11 +9,34 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
     {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          // {
+          //   resolve: `gatsby-remark-images`,
+          //   options: {
+          //     maxWidth: 400,
+          //   },
+          // },
+          `gatsby-remark-images`,
+        ],
+      },
+    },
+    {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         displayName: process.env.NODE_ENV === "production",
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-page-creator`,
       options: {
@@ -21,5 +44,36 @@ module.exports = {
         ignore: [`**/styles.js`],
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `BlogRoosevelt`,
+        short_name: `Blog`,
+        start_url: `/`,
+        background_color: `#f7f7f7`,
+        theme_color: `#FFCC57`,
+        display: `standalone`,
+        icon: `src/assets/icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#FFCC57`,
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [`300`, `500`, `700`],
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
