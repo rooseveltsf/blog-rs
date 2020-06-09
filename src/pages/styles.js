8d789040f821bf9bc068542e4259ed03869f543b
RooseveltSouza <rooseveltsouzav12@gmail.com>
2020-06-09 18:28:16 -0300

    fix: bug corrigido

--- src/pages/styles.js
+++ src/pages/styles.js
@@ -1,11 +1,11 @@
-import styled from "styled-components"
-import media from "styled-media-query"
-import Img from "gatsby-image"
-import { Link } from "gatsby"
+import styled from "styled-components";
+import media from "styled-media-query";
+import Img from "gatsby-image";
+import { Link } from "gatsby";
 
 export const Container = styled.div`
-  background: ${props => props.dark && "#3B3B3B"};
-  color: ${props => (props.dark ? "#f7f7f7" : "#3B3B3B")};
+  background: ${(props) => props.dark && "#3B3B3B"};
+  color: ${(props) => (props.dark ? "#f7f7f7" : "#3B3B3B")};
   height: 100vh;
 
   margin: auto;
@@ -14,7 +14,7 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-`
+`;
 
 export const ContainerHome = styled.div`
   flex: 1;
@@ -27,7 +27,7 @@ export const ContainerHome = styled.div`
   ${media.lessThan("small")`
     padding: 21px 10px 0;
   `}
-`
+`;
 
 export const Logo = styled(Img)`
   width: 301px;
@@ -37,13 +37,18 @@ export const Logo = styled(Img)`
     width: 150px;
     height: 60px;
   `}
-`
+`;
 
 export const ContainerData = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   margin-top: 100px;
+
+  ${media.lessThan("small")`
+    margin-top: 200px;
+  `}
 
   p {
     margin-top: 32px;
@@ -52,12 +57,12 @@ export const ContainerData = styled.div`
     line-height: 34px;
 
     ${media.lessThan("small")`
-    font-size: 16px;
+    font-size: 20px;
     line-height: 26px;
     text-align: justify;
   `}
   }
-`
+`;
 
 export const ContainerButtons = styled.div`
   display: flex;
@@ -69,39 +74,39 @@ export const ContainerButtons = styled.div`
     flex-direction: column;
     height: 200px;
   `}
-`
+`;
 
 export const PageProjects = styled(Link)`
   width: 50%;
   text-decoration: none;
-  color: ${props => (props.dark ? "#f7f7f7" : "#3B3B3B")};
+  color: ${(props) => (props.dark ? "#f7f7f7" : "#3B3B3B")};
   font-size: 40px;
   font-weight: bold;
 
   display: flex;
   align-items: center;
   justify-content: center;
-  background: ${props => (props.dark ? "#3B3B3B" : "#f7f7f7")};
+  background: ${(props) => (props.dark ? "#3B3B3B" : "#f7f7f7")};
 
   ${media.lessThan("small")`
     width: 100%;
     height: 100px;
   `}
-`
+`;
 export const PageAbout = styled(Link)`
   width: 50%;
   text-decoration: none;
-  color: ${props => (props.dark ? "#3B3B3B" : "#f7f7f7")};
+  color: ${(props) => (props.dark ? "#3B3B3B" : "#f7f7f7")};
   font-size: 40px;
   font-weight: bold;
 
   display: flex;
   align-items: center;
   justify-content: center;
-  background: ${props => (props.dark ? "#f7f7f7" : "#3B3B3B")};
+  background: ${(props) => (props.dark ? "#f7f7f7" : "#3B3B3B")};
 
   ${media.lessThan("small")`
     width: 100%;
     height: 100px;
   `}
-`
+`;
